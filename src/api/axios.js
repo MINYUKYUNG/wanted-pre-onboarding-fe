06f76c0ab9f796eb844e665f533c1a93a472bb76
MINYUKYUNG <minew0223@gmail.com>
2022-09-12 15:35:50 +0900

    🎨 Modify : api url 변경

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  // baseURL: 'https://5co7shqbsf.execute-api.ap-northeast-2.amazonaws.com/production/' // 변경 전
-  baseURL: 'https://n38lcff1wk.execute-api.ap-northeast-2.amazonaws.com' // 변경 후
+  baseURL: 'https://n38lcff1wk.execute-api.ap-northeast-2.amazonaws.com'
 });
